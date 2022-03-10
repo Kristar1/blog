b08d1e164131c9d600e042cdf5618f2123ec1e4a
Kristar1 <kediak32@gmail.com>
2022-03-10 09:37:29 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import '../styles/global.scss';
-import '../styles/global.css';
+import '../styles/globals.css';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
