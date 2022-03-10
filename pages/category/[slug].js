41f91e51df6476cac7624f5fbbfad4633cdc1231
Kristar1 <kediak32@gmail.com>
2022-03-10 10:08:19 +0530

    first commit

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -16,9 +16,9 @@ const CategoryPost = ({ posts }) => {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-             { posts?  posts.map((post, index) => (
+             {  posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
-          ))  :"No Posts"} 
+          )) } 
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
